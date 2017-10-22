ea2209e6893580a005d5f72dc13231a99062f7f6
soarflat <soarflat777@gmail.com>
2017-10-22 22:47:41 +0900

    [update] varをconstに

--- node-passport-social/app.js
+++ node-passport-social/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 
@@ -50,7 +50,7 @@ app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
